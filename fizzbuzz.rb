2e0baa54e8dd698947d3b543cefb5ccbb68a2a49
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-10 17:28:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(n)
-  if (n%3 ==0) && (n%5!==0)
+  if (n%3 ==0) && (n%5!=0)
     return "Fizz"
-  elsif (n%5 ==0) && (n%3!==0)
+  elsif (n%5 ==0) && (n%3!=0)
     return "Buzz"
   elsif (n%5 ==0) && (n%3==0)
     return "FizzBuzz"
